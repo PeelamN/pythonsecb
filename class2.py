6835014f9029f9b31d8718a50d3c5cb731671086
PeelamN <155879827+PeelamN@users.noreply.github.com>
2024-01-26 23:08:34 -0800

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,8 @@
+def isprime(self):
+    flag=True
+    if self.a > 1:
+        for val in range(2,self.a):
+            if self.a % val == 0:
+                flag =  False
+                break
+    return flag            
---
+++ comprehension.py
@@ -0,0 +1,6 @@
+def fact(a):
+    out=1
+    for _  in range(1,num+1):
+        out*=_
+        return out
+    
